1584b74a57c12601f3e6ba7fa17a20cd6904ff80
nosajmik <nosajmik@umich.edu>
2023-06-16 15:45:33 -0400

    Add setup script to run on boot for Asahi machines

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set +x
+set -Eeuo pipefail
+
+echo "Enabling kernel module..."
+cd /home/$USER/Desktop/m1-tools
+cd kmod
+sudo insmod kmod.ko
+
+echo "Enabling dc civac..."
+cd ../tools/chickenctl
+sudo ./target/release/chickenctl
+
+echo "Enabling cycle counter..."
+cd ../pmuctl
+sudo ./target/release/pmuctl
+
+echo "Pinning CPU to max frequency..."
+sudo cpupower frequency-set -g userspace
+sudo cpupower -c 0-3 frequency-set -f 9999999
+sudo cpupower -c 4-7 frequency-set -f 9999999
+
+echo "Testing all of the above and taskset..."
+cd /home/$USER/Desktop/asahi-cycle-counters
+taskset -c 5 ./cycles_and_flush
+
