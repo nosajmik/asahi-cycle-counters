b78ca57da6e7824c82f1c3fe2d6063a6131a24f8
nosajmik <nosajmik@umich.edu>
2023-09-12 19:31:31 -0400

    Works on E-cores, too

--- setup.sh
+++ setup.sh
@@ -23,6 +23,11 @@ sudo cpupower -c 4-7 frequency-set -f 9999999
 
 echo "Testing all of the above and taskset..."
 cd /home/$USER/Desktop/asahi-cycle-counters
+
+taskset -c 0 ./cycles_and_flush
+taskset -c 1 ./cycles_and_flush
+taskset -c 2 ./cycles_and_flush
+taskset -c 3 ./cycles_and_flush
 taskset -c 4 ./cycles_and_flush
 taskset -c 5 ./cycles_and_flush
 taskset -c 6 ./cycles_and_flush
